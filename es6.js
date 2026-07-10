95b68cc8935b72411f45475b31900a43f6d09de8
Aysel06 <mammedovaaysel0@gmail.com>
2026-07-10 13:18:19 +0400

    c
    Added a new movie to the movies array and filtered high-rated movies.

---
+++ es6.js
@@ -0,0 +1,26 @@
+const movies = [
+    { id: 1, title: "Interstellar", genre: "Sci-Fi", year: 2014, rating: 8.7 },
+    { id: 2, title: "Inception", genre: "Sci-Fi", year: 2010, rating: 8.8 },
+    { id: 3, title: "The Batman", genre: "Action", year: 2022, rating: 7.9 },
+    { id: 4, title: "Titanic", genre: "Drama", year: 1997, rating: 7.8 },
+    { id: 5, title: "Oppenheimer", genre: "Biography", year: 2023, rating: 8.5 },
+];
+
+const newMovies = [
+    ...movies,
+    {
+        id: 6,
+        title: "Avatar",
+        genre: "Sci-Fi",
+        year: 2009,
+        rating: 7.9
+    }    
+];
+
+const highRatedMovies = movies.filter(
+    (movie) =>  movie.rating >= 8
+);
+
+console.log(
+    `${movie.title} (${movie.year}) - Rating: ${movie.rating}`
+)
