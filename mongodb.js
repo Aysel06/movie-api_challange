520574f16ae8fe9f60799bd1c1dcede8309e61c5
Aysel06 <mammedovaaysel0@gmail.com>
2026-07-10 13:19:22 +0400

    a
    Added various MongoDB operations including insert, find, update, and delete for movies.

---
+++ mongodb.js
@@ -0,0 +1,50 @@
+db.movies.insertOne(
+    {
+        id: 6,
+        title: "Avatar",
+        genre: "Sci-Fi",
+        year: 2009,
+        rating: 7.9        
+    }
+)
+
+db.movies.find({})
+
+db.movies.find(
+    {
+    genre : "Sci-Fi"
+    }
+)
+
+db.movies.find(
+    {
+        rating: {
+            $gte : 8
+        }
+    }
+)
+
+db.movies.updateOne(
+    {
+        id : 3
+    },
+    {
+        $set : {
+        rating : 8.3
+    }
+    }
+)
+
+db.movies.deleteOne(
+    {
+        id : 4
+    }
+)
+
+db.movies.countDocuments({})
+
+db.movies.find({}).sort(
+    {
+        rating : -1
+    }
+)
